2688435236edc2d4218838929935ae3f11d17c81
Ashhad <syedashhad17@gmail.com>
2023-06-22 03:21:25 +0500

    card updated style 2

--- src/routes/CardStyle2/cardStructure.js
+++ src/routes/CardStyle2/cardStructure.js
@@ -3,27 +3,29 @@ import { Card } from "antd";
 
 const CardStructure = (props) => {
   const { userData } = props;
+  const gridStyle = {
+    width: "100%",
+    // textAlign: "center",
+  };
   return (
-    <div className="gx-d-flex justify-content-center">
-         <Card
-         key={userData.id}
-        title={userData.name}
-        bordered={true}
-        style={{
-          width: 300,
-          marginBottom: 10,
-          marginRight: 10,
-        }}
-      >
-        <ul>
-          <li>{userData.name}</li>
-          <li>{userData.username}</li>
-          <li>{userData.email}</li>
-          <li>{userData.phone}</li>
-          <li>{userData.website}</li>
-        </ul>
+
+      <Card title={userData.name} style={gridStyle}>
+        <Card.Grid >
+          <li>NAME: {userData.name}</li>
+        </Card.Grid>
+        <Card.Grid >
+          <li>USERNAME: {userData.username}</li>
+        </Card.Grid>
+        <Card.Grid >
+          <li>EMAIL: {userData.email}</li>
+        </Card.Grid>
+        <Card.Grid >
+          <li>PHONE: {userData.phone}</li>
+        </Card.Grid>
+        <Card.Grid >
+          <li>WEBSITE: {userData.website}</li>
+        </Card.Grid>
       </Card>
-    </div>
   );
 };
 
