074bc3e80fc4f668912be3cc8b31289924585b33
Ashhad <syedashhad17@gmail.com>
2023-06-22 03:35:51 +0500

    card Style 2 updated

--- src/routes/CardStyle2/cardStructure.js
+++ src/routes/CardStyle2/cardStructure.js
@@ -5,27 +5,39 @@ const CardStructure = (props) => {
   const { userData } = props;
   const gridStyle = {
     width: "100%",
-    // textAlign: "center",
+    textAlign: "center",
   };
+  console.log(userData);
   return (
-
-      <Card title={userData.name} style={gridStyle}>
-        <Card.Grid >
-          <li>NAME: {userData.name}</li>
-        </Card.Grid>
-        <Card.Grid >
-          <li>USERNAME: {userData.username}</li>
-        </Card.Grid>
-        <Card.Grid >
-          <li>EMAIL: {userData.email}</li>
-        </Card.Grid>
-        <Card.Grid >
-          <li>PHONE: {userData.phone}</li>
-        </Card.Grid>
-        <Card.Grid >
-          <li>WEBSITE: {userData.website}</li>
-        </Card.Grid>
-      </Card>
+    <Card title={userData.name} style={gridStyle}>
+      <Card.Grid>
+        <li><b>COMPANY</b>: {userData.company.name}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>NAME</b>: {userData.name}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>USERNAME</b>: {userData.username}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>EMAIL</b>: {userData.email}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>PHONE</b>: {userData.phone}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>WEBSITE</b>: {userData.website}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>ADDRESS/CITY</b>: {userData.address.city}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>STREET</b>: {userData.address.street}</li>
+      </Card.Grid>
+      <Card.Grid>
+        <li><b>ZIPCODE</b>: {userData.address.zipcode}</li>
+      </Card.Grid>
+    </Card>
   );
 };
 
